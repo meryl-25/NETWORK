49146a9e7bf6c389c45e9aca983c0d1fcb0e039b
Meryl John <149992491+meryl-25@users.noreply.github.com>
2024-07-08 21:13:21 +0530

    Delete SemExam directory

--- SemExam/evenodd.sh
+++
@@ -1,9 +0,0 @@
-echo "Enter a value for input"
-read a
-
-if((a % 2 == 0))
-then
-    echo $a "is an even number."
-else
-    echo $a "is an odd number."
-fi
--- SemExam/sum_salaries.sh
+++
@@ -1,9 +0,0 @@
-sum=0
-
-while read -r salary;
-do
-    ((sum = sum + salary))
-
-done < "salaries.txt"
-
-echo "Total sum of salaries : "$sum
